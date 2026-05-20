d4d384779a34f270c151fc5126358eabc3cecb87
GraphicitoDev <dev@graphicito.com>
2026-05-20 22:12:37 +0800

    Removed PWA Fixed route duplicate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   ssr: false,
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
+  experimental: {
+    appManifest: false,
+  },
+  modules: ["@nuxtjs/tailwindcss"],
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
   },
